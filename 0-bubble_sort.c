f06358ca7e5cf95259340c3e5b8a884121d62065
kareemwally <kareemwally@std.mans.edu.eg>
2024-01-27 17:21:55 -0800

    implementation in c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,26 +1,5 @@
 #include "sort.h"
 /**
- * print_array - Prints an array of integers
- *
- * @array: The array to be printed
- * @size: Number of elements in @array
- */
-void print_array(const int *array, size_t size)
-{
-	size_t i;
-
-	i = 0;
-	while (array && i < size)
-	{
-		if (i > 0)
-			printf(", ");
-		printf("%d", array[i])
-			++i;
-	}
-	printf("\n");
-}
-
-/**
  * bubble_sort - Function
  * Description: that function implements the bubble sort as seen
  * @array: the array of integers to be sorted
